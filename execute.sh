7339d91389ba4abb4a66f908dd34448d0567fba2
Adriano Baptistella <adrianojosebaptistella@gmail.com>
2017-09-28 15:24:25 -0300

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -4,21 +4,27 @@ JBOSS_HOME=/opt/jboss/wildfly
 JBOSS_CLI=$JBOSS_HOME/bin/jboss-cli.sh
 JBOSS_MODE=${1:-"standalone"}
 JBOSS_CONFIG=${2:-"$JBOSS_MODE.xml"}
+JBOSS_OPTS=$JBOSS_HOME/bin/$JBOSS_MODE.conf
 
 function wait_for_wildfly() {
-  until '$JBOSS_CLI -c "ls /deployment" &> /dev/null'; do
+  until `$JBOSS_CLI -c "ls /deployment" &> /dev/null`; do
     sleep 1
   done
 }
+#issue#1. Possible solution is to set user.timezone environment variable
+#echo "==> Patching Java TimeZone info..."
+#PATCH_STR='JAVA_OPTS="$JAVA_OPTS -Duser.timezone=UTC+02:00"'
+#todo: fix escaping for grep
+#grep -q -F 'JAVA_OPTS="$JAVA_OPTS -Duser.timezone=UTC+02:00"' $JBOSS_OPTS || echo "$PATCH_STR\n" >> $JBOSS_OPTS
 
 echo "==> Starting WildFly..."
-$JBOSS_HOME/bin/$JBOSS_MODE.sh -c $JBOSS_CONFIG &
+$JBOSS_HOME/bin/$JBOSS_MODE.sh -c $JBOSS_CONFIG > /dev/null &
 
 echo "==> Waiting..."
 wait_for_wildfly
 
 echo "==> Executing..."
-$JBOSS_CLI -c --file='dirname "$0"'/command.cli
+$JBOSS_CLI -c --file=`dirname "$0"`/command.cli
 
 echo "==> Shutting down WildFly..."
 if [ "$JBOSS_MODE" = "standalone" ]; then
